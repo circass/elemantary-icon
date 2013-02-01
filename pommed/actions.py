4eeb8e2fd14d5e3e36e78b93c3de4e1a1eddac77
circass <erdincgultekin@gmail.com>
2013-02-01 04:38:04 +0200

    pommed

--- pommed/actions.py
+++ pommed/actions.py
@@ -12,13 +12,22 @@ def build():
     autotools.make("-j1")
 
 def install():
-    pisitools.insinto("/usr/sbin", "/var/pisi/pommed-*/work/pommed-*/pommed/")
-    pisitools.insinto("/etc", "/var/pisi/pommed-*/work/pommed-*/pommed.conf.mactel")
-    pisitools.insinto("/etc", "/var/pisi/pommed-*/work/pommed-*/pommed.conf.pmac")
-    pisitools.insinto("/etc/dbus-1/system.d", "/var/pisi/pommed-*/work/pommed-*/dbus-policy.conf", "pommed.conf")
+    pisitools.insinto("/usr/bin", "pommed/")
+    pisitools.insinto("/etc", "pommed.conf.mactel")
+    pisitools.insinto("/etc", "pommed.conf.pmac")
+    pisitools.insinto("/etc/dbus-1/system.d", "dbus-policy.conf", "pommed.conf")
     # Man page
-    pisitools.insinto("/usr/share/man/man1", "/var/pisi/pommed-*/work/pommed-*/pommed.1")
+    pisitools.insinto("/usr/share/man/man1", "pommed.1")
     # Sounds
-    pisitools.insinto("/usr/share/pommed", "/var/pisi/pommed-*/work/pommed-*/pommed/data/*")
+    pisitools.insinto("/usr/share/pommed", "pommed/data/*")
+    #gpomme
+    pisitools.insinto("/usr/bin", "gpomme")
+    pisitools.insinto("/usr/share/gpomme/themes", "gpomme/themes/*")
+    pisitools.insinto("/usr/share/applications", "gpomme/gpomme*.desktop")
+    pisitools.insinto("/usr/share/icons/hicolor", "icons/gpomme.svg", "gpomme.svg")
+    #wmpomme
+    pisitools.insinto("/usr/bin", "wmpomme")
+    pisitools.insinto("/usr/share/pixmaps", "icons/gpomme_32x32.xpm", "wmpomme.xpm")
+    
 
     pisitools.dodoc("ChangeLog", "COPYING", "README")
